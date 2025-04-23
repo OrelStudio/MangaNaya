a798befb00420682254f64744e3efd88330b001d
OrelStudio <orel890x@gmail.com>
2025-04-23 18:29:30 +0300

    Adjusted stats size

--- apps/web/app/components/Genres/index.tsx
+++ apps/web/app/components/Genres/index.tsx
@@ -7,8 +7,8 @@ interface GenresProps {
 const Genres = ({genres}: GenresProps) => {
   return (
     <div className={styles.wrapper}>
-      {genres && genres.map((genre) => (
-        <div className={styles.genre} key={genre}>{genre}</div>
+      {genres && genres.map((genre, i) => (
+        i < 2 ? <div className={styles.genre} key={genre}>{genre}</div> : null
       ))}
       {!genres && (
         <div className={styles.empty}>Empty</div>
